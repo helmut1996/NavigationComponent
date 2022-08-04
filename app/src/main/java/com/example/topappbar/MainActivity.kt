79928f6fc83059450de64b56cc928098d58a87e2
hcolindres <hcolindres@empresa.thedataage.com>
2022-08-04 15:08:17 -0600

    ToolBar implementado con NavigationComponet

--- app/src/main/java/com/example/topappbar/MainActivity.kt
+++ app/src/main/java/com/example/topappbar/MainActivity.kt
@@ -2,14 +2,46 @@ package com.example.topappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.topappbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+        if (navHostFragment != null) {
+            navController  = navHostFragment.findNavController()
+        }
+
+        //Toolbar
+        setSupportActionBar(binding.Toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    //para habilitar la flecha de atras qeue se muestra en el toolbar
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) ||super.onOptionsItemSelected(item)
     }
 }
